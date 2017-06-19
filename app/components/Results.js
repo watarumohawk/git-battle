cb7207de0f320df081232ac42a9b6efc51cbad33
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2017-06-19 13:51:06 +0900

    sample results page

---
+++ app/components/Results.js
@@ -0,0 +1,14 @@
+var React = require('react');
+// var PropTypes = require('prop-types');
+
+class Results extends React.Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <div> Results</div>
+    );
+  }
+}
+
+module.exports = Results;
