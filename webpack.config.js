adfce964a3e1dd31587874878dbf89ca43e0dcf9
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2017-06-22 14:56:19 +0900

    setings to deploy on firebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var HtmlWebPackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 
-module.exports = {
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -21,4 +22,21 @@ module.exports = {
   plugins: [new HtmlWebPackPlugin({
     template: 'app/index.html'
   })]
+};
+
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+
+    new webpack.optimize.UglifyJsPlugin()
+  );
+
 }
+
+module.exports = config;
+
